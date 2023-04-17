78b691fb92bf3686bbe29dc7c3698899275163e4
William Wofford <27900656+Brungleby@users.noreply.github.com>
2023-04-17 11:20:52 -0500

    init serialization

---
+++ Runtime/Scripts/Objects/Serialization.cs
@@ -0,0 +1,46 @@
+
+/** Serialization.cs
+*
+*	Created by LIAM WOFFORD of CUBEROOT SOFTWARE, LLC.
+*
+*	Free to use or modify, with or without creditation,
+*	under the Creative Commons 0 License.
+*/
+
+#region Includes
+
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+#endregion
+
+namespace Cuberoot
+{
+	/// <summary>
+	/// __TODO_ANNOTATE__
+	///</summary>
+
+	public sealed class Serializer : object
+	{
+		#region Data
+
+		#region
+
+
+
+		#endregion
+
+		#endregion
+		#region Methods
+
+		#region
+
+
+
+		#endregion
+
+		#endregion
+	}
+}
