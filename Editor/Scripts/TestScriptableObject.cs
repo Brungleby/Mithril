449c0a5d6f25a255f3f2e0b147d6a5ee718c5c0f
William Wofford <27900656+Brungleby@users.noreply.github.com>
2023-04-18 01:39:18 -0500

    Implemented JSON string serialization

--- Editor/Scripts/TestScriptableObject.cs
+++ Editor/Scripts/TestScriptableObject.cs
@@ -28,196 +28,21 @@ namespace Cuberoot.Editor
 	/// __TODO_ANNOTATE__
 	///</summary>
 
-	public static class Serialization
-	{
-		public static void SerializeToFile<T>(this T obj, SerialSyntax syntax = SerialSyntax.Json)
-		where T : UnityEngine.Object
-		{
-			var __filePath = UnityEditor.AssetDatabase.GetAssetPath(obj);
-			SerializeToFile(obj, __filePath, syntax);
-		}
+	// public static class Serialization
+	// {
+	// 	public static void SerializeToFile<T>(this T obj, SerialSyntax syntax = SerialSyntax.Json)
+	// 	where T : UnityEngine.Object
+	// 	{
+	// 		var __filePath = UnityEditor.AssetDatabase.GetAssetPath(obj);
+	// 		SerializeToFile(obj, __filePath, syntax);
+	// 	}
 
-		public static void SerializeToFile<T>(this T obj, string filePath, SerialSyntax syntax = SerialSyntax.Json)
-		{
-			var __fileText = Serialize(obj, syntax);
-			File.WriteAllTextAsync(filePath, __fileText);
-		}
-
-		public static string Serialize<T>(this T obj, SerialSyntax syntax = SerialSyntax.Json)
-		{
-			switch (syntax)
-			{
-				case SerialSyntax.Json:
-					return _SerializeJson(obj);
-				default:
-					throw new NotImplementedException();
-			}
-
-		}
-
-		private static string _SerializeJson<T>(this T obj) =>
-			JsonUtility.ToJson(obj);
-
-		private static string _SerializeCube<T>(this T obj, string name, ref int depth)
-		{
-			string __result = string.Empty;
-
-			if (!obj._IsSerializable())
-			{
-				depth--;
-				throw new Exception();
-			}
-
-			__result += "{";
-			// __result += $"\"type\":\"{obj.GetTypeName()}\",";
-			__result += $"\"{name}\":{CURSOR}";
-			__result += "}";
-
-			string __subObject = string.Empty;
-
-			__subObject += "{";
-
-			// var fields = obj.GetFields();
-			// foreach (var iField in fields)
-			// {
-
-
-			// 	// __subObject +=
-			// }
-
-			// __subObject += "}";
-
-			return __result;
-		}
-		private static string _SerializeCube<T>(this T obj)
-		{
-			int __depth = 0;
-
-			return obj._SerializeCube("master", ref __depth);
-		}
-
-		private static bool _IsSerializable(this object obj)
-		{
-			return true;
-		}
-
-		private readonly static char CURSOR = '|';
-
-		private static string ReplaceCursorIn(string source, string insert)
-		{
-			var split = source.Split(CURSOR, 2);
-
-			return $"{split[0]}{insert}{split[1]}";
-		}
-
-		private static ETypeCategory GetTypeCategory(this object obj)
-		{
-			if (obj is IEnumerable)
-				return ETypeCategory.Array;
-			if (obj.GetType() == typeof(string) || obj.GetType() == typeof(char))
-				return ETypeCategory.String;
-			throw new NotImplementedException();
-		}
-
-		private enum ETypeCategory
-		{
-			Unbound,
-			String,
-			Array,
-			Struct,
-		}
-
-		private static string EncompassWithCurlybraces(this string input) =>
-			input.EncompassWith("{", "}");
-
-		private static string EncompassWithParentheses(this string input) =>
-			input.EncompassWith("(", ")");
-
-		private static string EncompassWithBrackets(this string input) =>
-			input.EncompassWith("[", "]");
-
-		private static string EncompassWithQuotes(this string input) =>
-			input.EncompassWith("\"", "\"");
-
-		private static string EncompassWith(this string input, string a, string b)
-		{
-			return $"{a}{input}{b}";
-		}
-	}
-
-	public class CubeSerializer
-	{
-		private bool _format;
-		private int _tabDepth = 0;
-
-		public CubeSerializer(bool format)
-		{
-			_format = format;
-		}
-
-		public string Serialize(object obj)
-		{
-			string __result = string.Empty;
-
-			__result += "{" + Line;
-
-			__result += $"\"Type\":\"{GetTypeName(obj)}\"," + Line;
-
-			var __fields = GetFieldInfo(obj);
-			foreach (var iField in __fields)
-			{
-				// __fields
-			}
-
-			__result += "}";
-
-			return __result;
-		}
-
-		private string Space =>
-			_format ? " " : string.Empty;
-
-		private string Line =>
-			_format ? "\n" + TabStart : string.Empty;
-
-		private string TabStart
-		{
-			get
-			{
-				if (!_format)
-					return string.Empty;
-
-				var __result = string.Empty;
-				for (var i = 0; i < _tabDepth; i++)
-				{
-				}
-
-				throw new NotImplementedException();
-			}
-		}
-
-		private void TabIn() =>
-			_tabDepth++;
-		private void TabOut() =>
-			_tabDepth--;
-
-		private string GetTypeName(object obj)
-		{
-			return obj.GetType().AssemblyQualifiedName;
-		}
-
-		private FieldInfo[] GetFieldInfo(object obj)
-		{
-			return obj.GetType().GetFields(BindingFlags.Instance | BindingFlags.NonPublic)
-				.Where(i => i.GetCustomAttributes(typeof(SerializeField), true).Any() || i.IsPublic)
-				.ToArray();
-		}
-	}
-
-	public enum SerialSyntax
-	{
-		Json
-	}
+	// 	public static void SerializeToFile<T>(this T obj, string filePath, SerialSyntax syntax = SerialSyntax.Json)
+	// 	{
+	// 		var __fileText = Serialize(obj, syntax);
+	// 		File.WriteAllTextAsync(filePath, __fileText);
+	// 	}
+	// }
 
 
 
@@ -267,7 +92,8 @@ namespace Cuberoot.Editor
 
 		public string Save()
 		{
-			return JsonUtility.ToJson(this, true);
+			// return JsonUtility.ToJson(this, true);
+			return Serializer.Serialize(this, false);
 
 		}
 
--- Runtime/Scripts/Objects/Serialization.cs
+++ Runtime/Scripts/Objects/Serialization.cs
@@ -9,6 +9,9 @@
 
 #region Includes
 
+using System;
+using System.Linq;
+using System.Reflection;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -28,6 +31,17 @@ namespace Cuberoot
 
 		#region
 
+		private readonly static string TYPE_LABEL = "type";
+		private readonly static string DATA_LABEL = "data";
+
+		private bool _prettyPrint = false;
+		private int _indentDepth = 0;
+
+		public int indentDepth
+		{
+			get => _indentDepth;
+			private set => _indentDepth = value.Max(0);
+		}
 
 
 		#endregion
@@ -35,9 +49,179 @@ namespace Cuberoot
 		#endregion
 		#region Methods
 
+		#region String Helpers
+
+		private string SPACE =>
+			_prettyPrint ? " " : string.Empty;
+
+		private string BLANKLINE =>
+			_prettyPrint ? "\n" : string.Empty;
+
+		private string NEWLINE =>
+			_prettyPrint ? BLANKLINE + TAB : string.Empty;
+
+		private string TAB
+		{
+			get
+			{
+				if (!_prettyPrint) return string.Empty;
+
+				var __result = string.Empty;
+				for (var i = 0; i < indentDepth; i++)
+					__result += "\t";
+				return __result;
+			}
+		}
+
+		private string INDENT_LINE
+		{
+			get
+			{
+				indentDepth++;
+				return NEWLINE;
+			}
+		}
+
+		private string OUTDENT_LINE
+		{
+			get
+			{
+				indentDepth--;
+				return NEWLINE;
+			}
+		}
+
+		private string OPEN_BRACE =>
+			"{" + INDENT_LINE;
+
+		private string CLOSE_BRACE =>
+			OUTDENT_LINE + "}";
+
+		private string OPEN_BRACKET =>
+			"[" + INDENT_LINE;
+
+		private string CLOSE_BRACKET =>
+			OUTDENT_LINE + "]";
+
+		private string ITERATE =>
+			"," + NEWLINE;
+
+		#endregion
+
 		#region
 
+		/// <summary>
+		/// Converts the given <paramref name="obj"/> into a serialized JSON string.
+		///</summary>
 
+		public string Serialize<T>(T obj) =>
+			Serialize(typeof(T), obj);
+
+		public static string Serialize<T>(T obj, bool prettyPrint)
+		{
+			var _ = new Serializer();
+			_._prettyPrint = prettyPrint;
+			return _.Serialize(obj);
+		}
+
+		private string Serialize(Type type, object obj)
+		{
+			var __result = string.Empty;
+			__result += OPEN_BRACE;
+
+			__result += $"\"{TYPE_LABEL}\":{SPACE}{Serialize(type)}" + ITERATE;
+			__result += $"\"{DATA_LABEL}\":{SPACE}{SerializeAny(type, obj)}";
+
+			__result += CLOSE_BRACE;
+			return __result;
+		}
+
+		private string SerializeAny(Type type, object obj)
+		{
+			if (obj.GetType() == typeof(string))
+				return Serialize((string)obj);
+
+			if (obj.GetType().IsArray)
+			{
+				object[] __arr = new object[((Array)obj).Length];
+				Array.Copy((Array)obj, __arr, __arr.Length);
+				return SerializeArray(type, __arr);
+			}
+
+			// if (type == typeof(ICollection))
+			// 	return Serialize((IEnumerable)((List<object>)obj).ToArray());
+
+			if (obj.GetType().IsPrimitive)
+				return obj.ToString();
+
+			return SerializeFields(type, obj);
+		}
+
+		private string SerializeFields(Type type, object obj)
+		{
+			var __result = string.Empty;
+			__result += OPEN_BRACE;
+
+			var __fields = new List<FieldInfo>();
+
+			__fields.AddAll(
+				type.GetFields(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public)
+				.Where(i =>
+					i.GetCustomAttributes(typeof(SerializeField), true).Any()
+					|| (i.Attributes & FieldAttributes.Public) != 0)
+				.ToArray()
+			);
+
+			for (var i = 0; i < __fields.Count; i++)
+			{
+				var iField = __fields[i];
+				__result += $"\"{iField.Name}\":{SPACE}{Serialize(iField, obj)}";
+
+				if (i != __fields.Count - 1)
+					__result += ITERATE;
+			}
+
+			__result += CLOSE_BRACE;
+			return __result;
+		}
+
+		private string SerializeArray(Type type, Array arr)
+		{
+			var __result = string.Empty;
+			__result += OPEN_BRACKET;
+
+			for (var i = 0; i < arr.Length; i++)
+			{
+				var iObject = arr.GetValue(i);
+				__result += Serialize(iObject.GetType(), iObject);
+
+				if (i != arr.Length - 1)
+					__result += ITERATE;
+			}
+
+			__result += CLOSE_BRACKET;
+			return __result;
+		}
+
+		private static string Serialize(string value) =>
+			$"\"{value}\"";
+
+		private static string Serialize(Type value) =>
+			Serialize(value.ToString());
+
+		private string Serialize(FieldInfo field, object parent) =>
+			Serialize(field.FieldType, field.GetValue(parent));
+
+
+
+		/// <summary>
+		/// Constructs a new object from the given serialized JSON <paramref name="data"/>.
+		///</summary>
+
+		public T Extract<T>(string data)
+		{
+			throw new NotImplementedException();
+		}
 
 		#endregion
 
