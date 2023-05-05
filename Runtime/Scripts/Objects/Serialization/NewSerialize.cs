aafb3be6034e80219b99086fae618cf4637cb467
William Wofford <27900656+Brungleby@users.noreply.github.com>
2023-05-05 01:54:20 -0500

    All new serialization passes

--- Runtime/Scripts/Objects/Serialization/NewSerialize.cs
+++ Runtime/Scripts/Objects/Serialization/NewSerialize.cs
@@ -11,6 +11,8 @@
 
 using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 
 #endregion
@@ -23,6 +25,20 @@ namespace Mithril
 
 	public sealed class NewSerialize : object
 	{
+		#region Inners
+
+		[System.Serializable]
+		public class WrapperDecodeException : System.Exception
+		{
+			public WrapperDecodeException() { }
+			public WrapperDecodeException(string message) : base(message) { }
+			public WrapperDecodeException(string message, System.Exception inner) : base(message, inner) { }
+			protected WrapperDecodeException(
+				System.Runtime.Serialization.SerializationInfo info,
+				System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+		}
+
+		#endregion
 		#region Data
 
 		#region Static
@@ -49,10 +65,43 @@ namespace Mithril
 		#endregion
 		#region Methods
 
-		#region Macro
+		#region Public
+
+		public static T Decode<T>(in string json) =>
+			(T)Decode(typeof(T), json);
+		public static object Decode(in string json) =>
+			Decode(typeof(object), json);
 
 		public static string Encode(object obj) =>
 			(new NewSerialize())._Encode(obj);
+
+		#endregion
+
+		#region Macro
+
+		public static object Decode(Type type, in string json)
+		{
+			if (RepresentsNull(json))
+				return null;
+
+			if (typeof(string) == type)
+				return DecodeString(json);
+
+			if (typeof(char) == type)
+				return DecodeChar(json);
+
+			if (type.IsEnum)
+				return DecodeEnum(type, json);
+
+			if (type.IsPrimitive)
+				return DecodePrimitive(type, json);
+
+			if (IsEncodedAsJsonArray(type))
+				return DecodeEnumerable(type, json);
+
+			return DecodeObject(type, json);
+			// throw new NotImplementedException($"Couldn't parse the following JSON as type {type}:\n\"{json}\"");
+		}
 
 		private string _Encode(object obj)
 		{
@@ -68,19 +117,129 @@ namespace Mithril
 				return EncodeChar((char)obj);
 
 			if (__type.IsEnum)
-				return EncodePrimitive((int)obj);
+				return EncodeEnum(obj);
 
 			if (__type.IsPrimitive)
 				return EncodePrimitive(obj);
 
-			if (IsEncodedAsArray(__type))
+			if (IsEncodedAsJsonArray(__type))
 				return EncodeArray(obj);
 
 			return EncodeObject(obj);
 		}
 
 		#endregion
-		#region String Arithmetic
+		#region Decode String Arithmetic
+
+		private static (string, string) Split(in string json, char q)
+		{
+			bool __isInQuotes = false;
+			int __contextDepth = 0;
+
+			for (var i = 0; i < json.Length; i++)
+			{
+				if (json[i] == '[' || json[i] == '{')
+				{
+					__contextDepth++;
+					continue;
+				}
+
+				if (json[i] == ']' || json[i] == '}')
+				{
+					__contextDepth--;
+					continue;
+				}
+
+				if (__contextDepth > 0)
+					continue;
+
+				if (json[i] == '\"' && (i == 0 || json[i - 1] != '\\'))
+				{
+					__isInQuotes = !__isInQuotes;
+					continue;
+				}
+
+				if (!__isInQuotes && json[i] == q)
+				{
+					return (json.Substring(0, i), json.Substring(i + 1));
+				}
+			}
+
+			throw new KeyNotFoundException();
+		}
+
+		private static string Unwrap(in string json, char start, char end)
+		{
+			var __start = json.IndexOf(start) + 1;
+			var __end = json.LastIndexOf(end);
+
+			try
+			{
+				return json.Substring(__start, __end - __start).Trim();
+			}
+			catch (Exception e)
+			{
+				if (__start == -1)
+					throw new IndexOutOfRangeException("Start character not found in the given data string.", e);
+				if (__end == -1)
+					throw new IndexOutOfRangeException("End character not found in the given data string.", e);
+
+				throw e;
+			}
+		}
+		private static string Unwrap(in string json, char startEnd) =>
+			Unwrap(json, startEnd, startEnd);
+
+		private static string[] UnwrapIterative(in string json, char start, char end)
+		{
+			var __json = Unwrap(json, start, end);
+
+			if (string.IsNullOrWhiteSpace(__json))
+				return new string[0];
+
+			var __result = new List<string>();
+
+		loop:
+			(string, string) __tuple;
+			try
+			{
+				__tuple = Split(__json, ',');
+			}
+			catch
+			{
+				__result.Add(__json);
+				return __result.ToArray();
+			}
+
+			__result.Add(__tuple.Item1);
+			__json = __tuple.Item2;
+
+			goto loop;
+		}
+
+		private static string[] UnwrapArray(in string json) =>
+			UnwrapIterative(json, '[', ']');
+
+		private static (string, string)[] UnwrapFieldPairs(in string json)
+		{
+			var __elements = UnwrapIterative(json, '{', '}');
+			var __result = new (string, string)[__elements.Length];
+
+			var i = 0;
+			foreach (var iString in __elements)
+			{
+				var iPair = Split(iString, ':');
+
+				__result[i] = (Unwrap(iPair.Item1, '\"'), iPair.Item2);
+
+				i++;
+			}
+
+			return __result;
+		}
+
+		#endregion
+		#region Encode String Arithmetic
 
 		private string SPACE =>
 			_prettyPrint ? " " : string.Empty;
@@ -139,7 +298,36 @@ namespace Mithril
 
 		#endregion
 
-		#region Primitive
+		#region Primitive | Enum | Null
+
+		private static bool RepresentsNull(in string json)
+		{
+			return json == NULL_ENCODED;
+		}
+
+		private static object DecodeEnum(Type type, in string json)
+		{
+			return Enum.Parse(type, json);
+		}
+
+		private static object DecodePrimitive(Type type, in string json)
+		{
+			var __m_Parse = type.GetMethod("Parse", new Type[] { typeof(string) });
+
+			try
+			{
+				return __m_Parse.Invoke(null, new object[] { json });
+			}
+			catch (Exception e)
+			{
+				throw new FormatException($"Couldn't parse the following JSON string to primitive type ({type}):\n\"{json}\"", e);
+			}
+		}
+
+		private string EncodeEnum(object obj)
+		{
+			return EncodePrimitive((int)obj);
+		}
 
 		private string EncodePrimitive(object obj)
 		{
@@ -153,15 +341,78 @@ namespace Mithril
 		private bool IsEscapeChar(char c) =>
 			JSON_ESCAPE_CHARS.Contains(c);
 
+		private static char DecodeChar(in string json)
+		{
+			/** <<============================================================>> **/
+
+			string __value;
+			try
+			{
+				__value = Unwrap(json, '\'');
+			}
+			catch (Exception e)
+			{
+				throw new FormatException($"The following JSON data could not be parsed as a char:\n\"{json}\"", e);
+			}
+
+			/** <<============================================================>> **/
+
+			if (__value[0] == '\\')
+				return GetEscapeChar_DecodeVersion(__value[1]);
+			return __value[0];
+		}
+
 		private string EncodeChar(char obj)
 		{
 			if (IsEscapeChar(obj))
-				return $"'\\{GetChar_NonEscapeVersion(obj)}'";
+				return $"'\\{GetEscapeChar_EncodeVersion(obj)}'";
 			return $"'{obj}'";
 		}
 
-		private char GetChar_NonEscapeVersion(char c)
+		private static char GetEscapeChar_DecodeVersion(char c)
 		{
+			switch (c)
+			{
+				case '\\':
+				case '\"':
+				case '\'':
+				case '/':
+					return c;
+			}
+
+			switch (c)
+			{
+				// case '0':
+				// 	return '\0';
+				// case 'a':
+				// 	return '\a';
+				case 'b':
+					return '\b';
+				case 'f':
+					return '\f';
+				case 'n':
+					return '\n';
+				case 'r':
+					return '\r';
+				case 't':
+					return '\t';
+					// case 'v':
+					// 	return '\v';
+			}
+
+			throw new FormatException($"'{c}' is not a valid JSON escape char.");
+		}
+
+		private char GetEscapeChar_EncodeVersion(char c)
+		{
+			switch (c)
+			{
+				case '\\':
+				case '\"':
+				case '\'':
+					// case '/':
+					return c;
+			}
 			switch (c)
 			{
 				// case '\0':
@@ -178,36 +429,77 @@ namespace Mithril
 					return 'r';
 				case '\t':
 					return 't';
-				// case '\v':
-				// 	return 'v';
-				default:
-					return c;
+					// case '\v':
+					// 	return 'v';
 			}
+
+			throw new FormatException($"'{c}' is not a valid JSON escape char.");
 		}
 
 		#endregion
 		#region String
 
-		private string EncodeString(string obj)
+		private static string DecodeString(in string json)
 		{
-			if (obj == null)
+			/** <<============================================================>> **/
+
+			string __value;
+			try
+			{
+				__value = Unwrap(json, '\"');
+			}
+			catch (Exception e)
+			{
+				throw new FormatException($"The following JSON data could not be parsed as a string:\n\"{json}\"", e);
+			}
+
+			/** <<============================================================>> **/
+
+			var __result = string.Empty;
+
+		loop:
+			var __quoteIndex = __value.IndexOf('\"');
+			var __escapeIndex = __value.IndexOf('\\');
+
+			if (__quoteIndex != -1 && (
+				__escapeIndex == -1 ||
+				__quoteIndex < __escapeIndex
+			))
+				throw new FormatException($"The following JSON string could not be parsed because it contains an unescaped quote (at index {__quoteIndex}):\n\"{json}\"");
+
+			if (__escapeIndex != -1)
+			{
+				__result += __value.Substring(0, __escapeIndex)
+					+ GetEscapeChar_DecodeVersion(__value[__escapeIndex + 1])
+				;
+				__value = __value.Substring(__escapeIndex + 2);
+				goto loop;
+			}
+
+			return __result + __value;
+		}
+
+		private string EncodeString(in string json)
+		{
+			if (json == null)
 				return "\"\"";
 
+			var __json = json;
 			var __result = string.Empty;
 
 			do
 			{
-				var __escapeIndex = obj.IndexOfAny(JSON_ESCAPE_CHARS);
+				var __escapeIndex = __json.IndexOfAny(JSON_ESCAPE_CHARS);
 
 				if (__escapeIndex > -1)
 				{
-					__result += $"{obj.Substring(0, __escapeIndex)}\\{GetChar_NonEscapeVersion(obj[__escapeIndex])}";
-					obj = obj.Substring(__escapeIndex + 1);
+					__result += $"{__json.Substring(0, __escapeIndex)}\\{GetEscapeChar_EncodeVersion(__json[__escapeIndex])}";
+					__json = __json.Substring(__escapeIndex + 1);
 
 					continue;
 				}
 
-				__result += obj;
+				__result += __json;
 				break;
 			} while (true);
 
@@ -218,9 +510,60 @@ namespace Mithril
 
 		#region Array
 
-		private bool IsEncodedAsArray(Type type)
+		private static bool IsDecodedAsArray(Type type) =>
+			typeof(Array).IsAssignableFrom(type);
+
+		private static bool IsDecodedAsCollection_T_(Type type)
 		{
-			return type.GetInterfaces().Contains(typeof(IEnumerable));
+			try
+			{
+				return type.GetGenericTypeDefinition().GetInterface(typeof(ICollection<>).Name) != null;
+			}
+			catch
+			{
+				return false;
+			}
+		}
+
+		private static bool IsEncodedAsJsonArray(Type type) =>
+			type.GetInterfaces().Contains(typeof(IEnumerable));
+
+		private static object DecodeEnumerable(Type type, in string json)
+		{
+			if (IsDecodedAsArray(type))
+				return DecodeArray(type.GetElementType(), json);
+
+			if (IsDecodedAsCollection_T_(type))
+				return DecodeCollection_T_(type, json);
+
+			throw new NotImplementedException($"The data structure ({type.Name}) is not currently able to be decoded from a json array.");
+		}
+
+		private static Array DecodeArray(Type elementType, in string json)
+		{
+			var __elementStrings = UnwrapArray(json);
+
+			var __result = Array.CreateInstance(elementType, __elementStrings.Length);
+
+			for (var i = 0; i < __result.Length; i++)
+				__result.SetValue(Decode(elementType, __elementStrings[i]), i);
+
+			return __result;
+		}
+
+		private static object DecodeCollection_T_(Type type, in string json)
+		{
+			var __result = Activator.CreateInstance(type);
+
+			var __elementType = type.GetGenericArguments()[0];
+			var __elements = DecodeArray(__elementType, json);
+
+			var __m_Add = type.GetMethod("Add", new Type[] { __elementType });
+
+			foreach (var iElement in __elements)
+				__m_Add.Invoke(__result, new object[] { iElement });
+
+			return __result;
 		}
 
 		private string EncodeArray(object obj)
@@ -245,8 +588,55 @@ namespace Mithril
 		#endregion
 		#region Object
 
+		private static object DecodeObject(Type expectedType, in string json)
+		{
+			/** <<============================================================>> **/
+
+			var __wrapperFields = UnwrapFieldPairs(json);
+
+			if (__wrapperFields.Length == 0)
+				return new object();
+
+			if (
+				__wrapperFields.Length != 2 ||
+				__wrapperFields[0].Item1 != TYPE_LABEL ||
+				__wrapperFields[1].Item1 != DATA_LABEL
+			)
+				throw new WrapperDecodeException($"The following JSON string does not properly represent an object value because its wrapper fields are missing or incorrect:\n\"{json}\"");
+
+			/** <<============================================================>> **/
+
+			var __objectType = DecodeType(__wrapperFields[0].Item2);
+			var __objectFieldPairs = UnwrapFieldPairs(__wrapperFields[1].Item2);
+
+			var __result = Activator.CreateInstance(__objectType);
+
+			foreach (var iPair in __objectFieldPairs)
+			{
+				var iField = __objectType.GetField(iPair.Item1);
+				var iValue = Decode(iField.FieldType, iPair.Item2);
+
+				iField.SetValue(__result, iValue);
+			}
+
+			return __result;
+		}
+
+		private static Type DecodeType(in string json)
+		{
+			return Type.GetType(Unwrap(json, '\"'));
+		}
+
 		private string EncodeObject(object obj)
 		{
+			if (typeof(object) == obj.GetType())
+			{
+#if !UNITY_INCLUDE_TESTS
+				UnityEngine.Debug.LogWarning("A pure, non-null object was encoded to json.");
+#endif
+				return "{}";
+			}
+
 			var __result = OPEN_BRACE;
 
 			__result += $"\"{TYPE_LABEL}\":{SPACE}{EncodeType(obj.GetType())}";
--- Tests/Mithril.Core.Tests.EditMode/Scripts/Serialization_Tests.cs
+++ Tests/Mithril.Core.Tests.EditMode/Scripts/Serialization_Tests.cs
@@ -4,21 +4,65 @@
 
 #region Includes
 
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
 using NUnit.Framework;
 
-using UnityEngine;
-using UnityEngine.TestTools;
-
-using Mithril;
-
 #endregion
 
 namespace Mithril.Tests.Serialization
 {
+	#region Objects
+
+	public class Pizza
+	{
+		public float diameter;
+
+		public override bool Equals(object obj)
+		{
+			if (obj != null && obj is Pizza that)
+			{
+				return this.diameter == that.diameter;
+			}
+
+			return false;
+		}
+
+		public override int GetHashCode()
+		{
+			unchecked
+			{
+				var hash = 17;
+				hash = hash * 31 + diameter.GetHashCode();
+				return hash;
+			}
+		}
+	}
+
+	public class ThinPizza : Pizza
+	{
+
+	}
+
+	public class DeepDish : Pizza
+	{
+		public float greasiness;
+	}
+
+	public enum EToppinType
+	{
+		Mushroom = 1,
+		Cheese = 2,
+		Tomato = 4,
+		Sausage = 8,
+		Pineapple = 16,
+	}
+
+	#endregion
+
 	#region EncodeValue
 
 	public class EncodeValue
@@ -176,18 +220,6 @@ namespace Mithril.Tests.Serialization
 			/**	<<==  ASSERT   ==>>	**/
 
 			Assert.AreEqual(__expected, NewSerialize.Encode(__object));
-		}
-
-		#endregion
-		#region Objects
-
-		private enum EToppinType
-		{
-			Mushroom = 1,
-			Cheese = 2,
-			Tomato = 4,
-			Sausage = 8,
-			Pineapple = 16,
 		}
 
 		#endregion
@@ -375,11 +407,11 @@ namespace Mithril.Tests.Serialization
 	public class EncodeObject
 	{
 		[Test]
-		public void IsWrapperWithBracesOnly_BasicSystemObject()
+		public void IsBracesOnly_PureObject()
 		{
 			/**	<<==  ARRANGE  ==>>	**/
 
-			var __expected = "{" + $"\"{NewSerialize.TYPE_LABEL}\":\"{typeof(object).AssemblyQualifiedName}\",\"{NewSerialize.DATA_LABEL}\":" + "{}}";
+			var __expected = "{}";
 			var __object = new object();
 
 			/**	<<==  ACT      ==>>	**/
@@ -413,18 +445,628 @@ namespace Mithril.Tests.Serialization
 
 	#endregion
 
-	public class Pizza
+	#region DecodeValue
+
+	public class DecodeValue
 	{
-		public float diameter;
+		[Test]
+		public void IsNull_NullJson()
+		{
+			/**	<<==  ARRANGE  ==>>	**/
+			var __json = "null";
+
+
+			/**	<<==  ACT      ==>>	**/
+
+
+
+			/**	<<==  ASSERT   ==>>	**/
+
+			Assert.IsNull(NewSerialize.Decode<object>(__json));
+		}
+
+		[Test]
+		public void IsBoolTrue_BoolJson_True()
+		{
+			/**	<<==  ARRANGE  ==>>	**/
+			var __expected = true;
+			var __json = "true";
+
+			/**	<<==  ACT      ==>>	**/
+
+
+
+			/**	<<==  ASSERT   ==>>	**/
+
+			Assert.AreEqual(__expected, NewSerialize.Decode<bool>(__json));
+		}
+
+		[Test]
+		public void IsInteger8_IntegerJson_8()
+		{
+			/**	<<==  ARRANGE  ==>>	**/
+
+			var __expected = 8;
+			var __json = "8";
+
+			/**	<<==  ACT      ==>>	**/
+
+
+
+			/**	<<==  ASSERT   ==>>	**/
+
+			Assert.AreEqual(__expected, NewSerialize.Decode<int>(__json));
+		}
+
+		[Test]
+		public void IsIntegerNeg25_IntegerJson_Neg25()
+		{
+			/**	<<==  ARRANGE  ==>>	**/
+
+			var __expected = -25;
+			var __json = "-25";
+
+			/**	<<==  ACT      ==>>	**/
+
+
+
+			/**	<<==  ASSERT   ==>>	**/
+
+			Assert.AreEqual(__expected, NewSerialize.Decode<int>(__json));
+		}
+
+		[Test]
+		public void IsFloat10p5_FloatJson_10p5()
+		{
+			/**	<<==  ARRANGE  ==>>	**/
+
+			var __expected = 10.5f;
+			var __json = "10.5";
+
+			/**	<<==  ACT      ==>>	**/
+
+
+
+			/**	<<==  ASSERT   ==>>	**/
+
+			Assert.AreEqual(__expected, NewSerialize.Decode<float>(__json));
+		}
+
+		[Test]
+		public void ThrowsFormatException_IntegerJson_Pumpkin()
+		{
+			/**	<<==  ARRANGE  ==>>	**/
+
+			var __json = "Pumpkin";
+
+			/**	<<==  ACT      ==>>	**/
+
+
+
+			/**	<<==  ASSERT   ==>>	**/
+
+			Assert.Throws<FormatException>(() => { NewSerialize.Decode<int>(__json); });
+		}
+
+		[Test]
+		public void IsPineapple_EnumJson_16()
+		{
+			/**	<<==  ARRANGE  ==>>	**/
+
+			var __expected = EToppinType.Pineapple;
+			var __json = "16";
+
+			/**	<<==  ACT      ==>>	**/
+
+			var __result = NewSerialize.Decode<EToppinType>(__json);
+
+
+			/**	<<==  ASSERT   ==>>	**/
+
+			Assert.AreEqual(typeof(EToppinType), __result.GetType());
+			Assert.AreEqual(__expected, __result);
+		}
+
+		[Test]
+		public void IsEmptyString_StringJson_QuotesOnly()
+		{
+			/**	<<==  ARRANGE  ==>>	**/
+
+			var __expected = string.Empty;
+			var __json = "\"\"";
+
+			/**	<<==  ACT      ==>>	**/
+
+
+
+			/**	<<==  ASSERT   ==>>	**/
+
+			Assert.AreEqual(__expected, NewSerialize.Decode<string>(__json));
+		}
+
+		[Test]
+		public void IsAsExpected_StringJson_PumpkinWithQuotes()
+		{
+			/**	<<==  ARRANGE  ==>>	**/
+
+			var __expected = "Pumpkin";
+			var __json = "\"Pumpkin\"";
+
+			/**	<<==  ACT      ==>>	**/
+
+
+
+			/**	<<==  ASSERT   ==>>	**/
+
+			Assert.AreEqual(__expected, NewSerialize.Decode<string>(__json));
+		}
+
+		[Test]
+		public void ThrowsFormatException_StringJson_PumpkinWithNoQuotes()
+		{
+			/**	<<==  ARRANGE  ==>>	**/
+
+			var __json = "Pumpkin";
+
+			/**	<<==  ACT      ==>>	**/
+
+
+
+			/**	<<==  ASSERT   ==>>	**/
+
+			Assert.Throws<FormatException>(() => { NewSerialize.Decode<string>(__json); });
+		}
+
+		[Test]
+		public void ThrowsFormatException_StringJson_PumpkinWithOnlyOpenQuote()
+		{
+			/**	<<==  ARRANGE  ==>>	**/
+
+			var __json = "\"Pumpkin";
+
+			/**	<<==  ACT      ==>>	**/
+
+
+
+			/**	<<==  ASSERT   ==>>	**/
+
+			Assert.Throws<FormatException>(() => { NewSerialize.Decode<string>(__json); });
+		}
+
+		[Test]
+		public void ThrowsFormatException_StringJson_PumpkinWithOnlyOneQuote()
+		{
+			/**	<<==  ARRANGE  ==>>	**/
+
+			var __json = "Pumpkin\"";
+
+			/**	<<==  ACT      ==>>	**/
+
+
+
+			/**	<<==  ASSERT   ==>>	**/
+
+			Assert.Throws<FormatException>(() => { NewSerialize.Decode<string>(__json); });
+		}
+
+		[Test]
+		public void ThrowsFormatException_StringJson_PumpkinWithTripleQuote()
+		{
+			/**	<<==  ARRANGE  ==>>	**/
+
+			var __json = "\"Pump\"kin\"";
+
+			/**	<<==  ACT      ==>>	**/
+
+
+
+			/**	<<==  ASSERT   ==>>	**/
+
+			Assert.Throws<FormatException>(() => { NewSerialize.Decode<string>(__json); });
+		}
+
+		[Test]
+		public void ThrowsFormatException_StringJson_PumpkinWithEscapePrecedingQuote()
+		{
+			/**	<<==  ARRANGE  ==>>	**/
+
+			var __json = "\"Pump\\kin\"Pie\"";
+
+			/**	<<==  ACT      ==>>	**/
+
+
+
+			/**	<<==  ASSERT   ==>>	**/
+
+			Assert.Throws<FormatException>(() => { NewSerialize.Decode<string>(__json); });
+		}
+
+		[Test]
+		public void ThrowsNotFormatException_StringJson_PumpkinWithEscapeQuote()
+		{
+			/**	<<==  ARRANGE  ==>>	**/
+
+			var __expected = "Pump\"kin";
+			var __json = "\"Pump\\\"kin\"";
+			string __result;
+
+			/**	<<==  ACT      ==>>	**/
+
+
+
+			/**	<<==  ASSERT   ==>>	**/
+
+			Assert.DoesNotThrow(() =>
+			{
+				__result = NewSerialize.Decode<string>(__json);
+				Assert.AreEqual(__expected, __result);
+			});
+		}
+
+		[Test]
+		public void ThrowsNotFormatException_StringJson_PumpkinWithEscapeOnly()
+		{
+			/**	<<==  ARRANGE  ==>>	**/
+
+			var __expected = "Pump\\kin";
+			var __json = "\"Pump\\\\kin\"";
+			string __result;
+
+			/**	<<==  ACT      ==>>	**/
+
+
+
+			/**	<<==  ASSERT   ==>>	**/
+
+			Assert.DoesNotThrow(() =>
+			{
+				__result = NewSerialize.Decode<string>(__json);
+				Assert.AreEqual(__expected, __result);
+			});
+		}
+
+		[Test]
+		public void ThrowsFormatException_StringJson_PumpkinWithIncompleteEscape()
+		{
+			/**	<<==  ARRANGE  ==>>	**/
+
+			var __json = "\"Pump\\kin\"";
+			string __result;
+
+			/**	<<==  ACT      ==>>	**/
+
+
+
+			/**	<<==  ASSERT   ==>>	**/
+
+			Assert.Throws<FormatException>(() => { __result = NewSerialize.Decode<string>(__json); });
+		}
+
+		[Test]
+		public void IsAsExpected_CharJson_A()
+		{
+			/**	<<==  ARRANGE  ==>>	**/
+
+			var __expected = 'A';
+			var __json = "\'A\'";
+
+			/**	<<==  ACT      ==>>	**/
+
+
+
+			/**	<<==  ASSERT   ==>>	**/
+
+			Assert.AreEqual(__expected, NewSerialize.Decode<char>(__json));
+		}
+
+		[Test]
+		public void IsAsExpected_CharJson_DoubleQuote()
+		{
+			/**	<<==  ARRANGE  ==>>	**/
+
+			var __expected = '\"';
+			var __json = "\'\"\'";
+
+			/**	<<==  ACT      ==>>	**/
+
+
+
+			/**	<<==  ASSERT   ==>>	**/
+
+			Assert.AreEqual(__expected, NewSerialize.Decode<char>(__json));
+		}
+
+		[Test]
+		public void IsAsExpected_CharJson_Slash()
+		{
+			/**	<<==  ARRANGE  ==>>	**/
+
+			var __expected = '/';
+			var __json = "\'/\'";
+
+			/**	<<==  ACT      ==>>	**/
+
+
+
+			/**	<<==  ASSERT   ==>>	**/
+
+			Assert.AreEqual(__expected, NewSerialize.Decode<char>(__json));
+		}
+
+		[Test]
+		public void IsAsExpected_CharJson_EscapeSlash()
+		{
+			/**	<<==  ARRANGE  ==>>	**/
+
+			var __expected = '/';
+			var __json = "\'\\/\'";
+
+			/**	<<==  ACT      ==>>	**/
+
+
+
+			/**	<<==  ASSERT   ==>>	**/
+
+			Assert.AreEqual(__expected, NewSerialize.Decode<char>(__json));
+		}
+
 	}
 
-	public class ThinPizza : Pizza
-	{
+	#endregion
+	#region DecodeEnumerable
 
+	public class DecodeEnumerable
+	{
+		[Test]
+		public void IsEmptyArray_PureArrayJson_BracketsOnly()
+		{
+			/**	<<==  ARRANGE  ==>>	**/
+
+			var __expected = new object[0];
+			var __json = "[]";
+
+			/**	<<==  ACT      ==>>	**/
+
+
+
+			/**	<<==  ASSERT   ==>>	**/
+
+			Assert.AreEqual(__expected, NewSerialize.Decode<object[]>(__json));
+		}
+
+		[Test]
+		public void IsAsExpected_IntegerArrayJson_BracketsOnly()
+		{
+			/**	<<==  ARRANGE  ==>>	**/
+
+			var __expected = new int[0];
+			var __json = "[]";
+
+			/**	<<==  ACT      ==>>	**/
+
+
+
+			/**	<<==  ASSERT   ==>>	**/
+
+			Assert.AreEqual(__expected, NewSerialize.Decode<int[]>(__json));
+		}
+
+		[Test]
+		public void IsAsExpected_IntegerArrayJson_0_n2_300()
+		{
+			/**	<<==  ARRANGE  ==>>	**/
+
+			var __expected = new int[] { 0, -2, 300 };
+			var __json = "[0,-2,300]";
+
+			/**	<<==  ACT      ==>>	**/
+
+
+
+			/**	<<==  ASSERT   ==>>	**/
+
+			Assert.AreEqual(__expected, NewSerialize.Decode<int[]>(__json));
+		}
+
+		[Test]
+		public void IsEmptyList_EmptyArrayJson()
+		{
+			/**	<<==  ARRANGE  ==>>	**/
+
+			var __expected = new List<object>();
+			var __json = "[]";
+
+			/**	<<==  ACT      ==>>	**/
+
+
+
+			/**	<<==  ASSERT   ==>>	**/
+
+			Assert.AreEqual(__expected, NewSerialize.Decode<List<object>>(__json));
+		}
+
+		[Test]
+		public void IsAsList_IntegerArrayJson_0_n2_300()
+		{
+			/**	<<==  ARRANGE  ==>>	**/
+
+			var __expected = new List<int>() { 0, -2, 300 };
+			var __json = "[0,-2,300]";
+
+			/**	<<==  ACT      ==>>	**/
+
+
+
+			/**	<<==  ASSERT   ==>>	**/
+
+			Assert.AreEqual(__expected, NewSerialize.Decode<List<int>>(__json));
+		}
+
+		[Test]
+		public void IsAsHashSet_StringArrayJson_Length5()
+		{
+			/**	<<==  ARRANGE  ==>>	**/
+
+			var __expected = new HashSet<string>() { "Mushroom", "Cheese", "Tomato", "Sausage", "Pineapple" };
+			var __json = "[\"Mushroom\",\"Cheese\",\"Tomato\",\"Sausage\",\"Pineapple\"]";
+
+			/**	<<==  ACT      ==>>	**/
+
+
+
+			/**	<<==  ASSERT   ==>>	**/
+
+			Assert.AreEqual(__expected, NewSerialize.Decode<HashSet<string>>(__json));
+		}
+
+		[Test]
+		public void ThrowsNotImplementedException_Hashtable()
+		{
+			/**	<<==  ARRANGE  ==>>	**/
+
+			var __json = "[]";
+
+			/**	<<==  ACT      ==>>	**/
+
+
+
+			/**	<<==  ASSERT   ==>>	**/
+
+			Assert.Throws<NotImplementedException>(() => { NewSerialize.Decode<Hashtable>(__json); });
+		}
 	}
 
-	public class DeepDish : Pizza
+	#endregion
+	#region DecodeObject
+
+	public class DecodeObject
 	{
-		public float greasiness;
+		[Test]
+		public void IsPureObject_BracesOnly()
+		{
+			/**	<<==  ARRANGE  ==>>	**/
+
+			var __expected = new object();
+			var __json = "{}";
+
+			/**	<<==  ACT      ==>>	**/
+
+
+
+			/**	<<==  ASSERT   ==>>	**/
+
+			Assert.AreEqual(__expected.GetType(), NewSerialize.Decode(__json).GetType());
+		}
+
+		[Test]
+		public void Pizza_Equals_PizzaWithMatchingDiameter()
+		{
+			/**	<<==  ARRANGE  ==>>	**/
+
+			var __a = new Pizza() { diameter = 5f };
+			var __b = new Pizza() { diameter = 5f };
+
+			/**	<<==  ACT      ==>>	**/
+
+
+
+			/**	<<==  ASSERT   ==>>	**/
+
+			Assert.AreEqual(__a, __b);
+		}
+
+		[Test]
+		public void Pizza_NotEquals_PizzaWithDifferingDiameter()
+		{
+			/**	<<==  ARRANGE  ==>>	**/
+
+			var __a = new Pizza() { diameter = 5f };
+			var __b = new Pizza() { diameter = 10f };
+
+			/**	<<==  ACT      ==>>	**/
+
+
+
+			/**	<<==  ASSERT   ==>>	**/
+
+			Assert.AreNotEqual(__a, __b);
+		}
+
+		[Test]
+		public void IsAsExpected_PizzaJson()
+		{
+			/**	<<==  ARRANGE  ==>>	**/
+
+			var __expected = new Pizza() { diameter = 27f };
+			var __json = "{" + $"\"{NewSerialize.TYPE_LABEL}\":\"{typeof(Pizza).AssemblyQualifiedName}\",\"{NewSerialize.DATA_LABEL}\":" + "{\"diameter\":27}}";
+
+			/**	<<==  ACT      ==>>	**/
+
+
+
+			/**	<<==  ASSERT   ==>>	**/
+
+			Assert.AreEqual(__expected, NewSerialize.Decode(__json));
+		}
+
+		[Test]
+		public void ThrowsWrapperException_PizzaJson_WithExtraWrapperFields()
+		{
+			/**	<<==  ARRANGE  ==>>	**/
+
+			var __json = "{" + $"\"{NewSerialize.TYPE_LABEL}\":\"{typeof(Pizza).AssemblyQualifiedName}\",\"{NewSerialize.DATA_LABEL}\":" + "{\"diameter\":27}," + "\"BACON\":\"GREASE\"" + "}";
+
+			/**	<<==  ACT      ==>>	**/
+
+
+
+			/**	<<==  ASSERT   ==>>	**/
+
+			Assert.Throws<NewSerialize.WrapperDecodeException>(() =>
+			{
+				NewSerialize.Decode(__json);
+			});
+		}
+
+		[Test]
+		public void ThrowsWrapperException_PizzaJson_WithIncorrectWrapperFields()
+		{
+			/**	<<==  ARRANGE  ==>>	**/
+
+			var __json = "{" + $"\"FART\":\"{typeof(Pizza).AssemblyQualifiedName}\",\"STINKY\":" + "{\"diameter\":27}}";
+
+			/**	<<==  ACT      ==>>	**/
+
+
+
+			/**	<<==  ASSERT   ==>>	**/
+
+			Assert.Throws<NewSerialize.WrapperDecodeException>(() =>
+			{
+				NewSerialize.Decode(__json);
+			});
+		}
+
+		[Test]
+		public void ThrowsInvalidCastException_DecodeThinPizza_AsDeepDish()
+		{
+			/**	<<==  ARRANGE  ==>>	**/
+
+			var __json = "{" + $"\"{NewSerialize.TYPE_LABEL}\":\"{typeof(ThinPizza).AssemblyQualifiedName}\",\"{NewSerialize.DATA_LABEL}\":" + "{\"diameter\":27}}";
+
+
+			/**	<<==  ACT      ==>>	**/
+
+
+
+			/**	<<==  ASSERT   ==>>	**/
+
+			Assert.Throws<InvalidCastException>(() =>
+			{
+				NewSerialize.Decode<DeepDish>(__json);
+			});
+		}
 	}
+
+	#endregion
 }
