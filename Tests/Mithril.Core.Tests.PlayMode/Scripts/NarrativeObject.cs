e3b83be2d40145eaf4245ca8e01686403c830ffe
William Wofford <27900656+Brungleby@users.noreply.github.com>
2023-05-16 17:41:34 -0500

    fixup

---
+++ Tests/Mithril.Core.Tests.PlayMode/Scripts/NarrativeObject.cs
@@ -0,0 +1,46 @@
+
+/** NarrativeObject.cs
+*
+*	Created by LIAM WOFFORD of CUBEROOT SOFTWARE, LLC.
+*
+*	Free to use or modify, with or without creditation,
+*	under the Creative Commons 0 License.
+*/
+
+#region Includes
+
+using UnityEngine;
+
+#endregion
+
+namespace Mithril
+{
+	/// <summary>
+	/// __TODO_ANNOTATE__
+	///</summary>
+
+	public sealed class NarrativeObject : EditableObject
+	{
+		#region Data
+
+		#region
+
+
+
+		#endregion
+
+		#endregion
+		#region Methods
+
+		#region
+
+		public NarrativeObject()
+		{
+
+		}
+
+		#endregion
+
+		#endregion
+	}
+}
