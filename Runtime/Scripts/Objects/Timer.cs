7f9be7282df5a2566bd0c029eafcdd421f9c6b19
William Wofford <27900656+Brungleby@users.noreply.github.com>
2023-05-25 14:04:58 -0500

    Updated Timer

--- Runtime/Scripts/Objects/Timer.cs
+++ Runtime/Scripts/Objects/Timer.cs
@@ -13,9 +13,10 @@ using System;
 
 using System.Collections;
 using System.Collections.Generic;
-
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UIElements;
 
 #endregion
 
@@ -29,13 +30,80 @@ namespace Mithril
 
 	[Serializable]
 
-	public class Timer
+	public sealed class Timer : object
 	{
+		#region Inners
+
+		#region Editor
+#if UNITY_EDITOR
+		[CustomPropertyDrawer(typeof(Timer))]
+		private sealed class PropertyDrawer : UnityEditor.PropertyDrawer
+		{
+			public static readonly float PROPERTY_SPACING = 3f;
+
+			public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+			{
+				EditorGUI.BeginProperty(position, label, property);
+
+				/** <<============================================================>> **/
+
+				var loopsProperty = property.FindPropertyRelative("isLooping");
+				var curveProperty = property.FindPropertyRelative("curve");
+				var valueProperty = property.FindPropertyRelative("duration");
+
+				/** <<============================================================>> **/
+
+				var loopsTooltip = new GUIContent(string.Empty, loopsProperty.tooltip);
+				var curveTooltip = new GUIContent(string.Empty, curveProperty.tooltip);
+				var valueTooltip = new GUIContent(string.Empty, valueProperty.tooltip);
+
+				/** <<============================================================>> **/
+
+				position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+
+				float loopsWidth = 14f;
+
+				float curveLeft = loopsWidth + PROPERTY_SPACING;
+
+				float valueWidth = 30f;
+				float valueLeft = position.width - valueWidth;
+
+				float curveWidth = position.width - loopsWidth - valueWidth - PROPERTY_SPACING * 2f;
+
+				Rect loopsRect = new Rect(position.x, position.y, loopsWidth, position.height);
+				Rect curveRect = new Rect(position.x + curveLeft, position.y, curveWidth, position.height);
+				Rect valueRect = new Rect(position.x + valueLeft, position.y, valueWidth, position.height);
+
+				/** <<============================================================>> **/
+
+				EditorGUI.PropertyField(loopsRect, loopsProperty, loopsTooltip);
+				EditorGUI.PropertyField(curveRect, curveProperty, curveTooltip);
+				EditorGUI.PropertyField(valueRect, valueProperty, valueTooltip);
+
+				/** <<============================================================>> **/
+
+				EditorGUI.EndProperty();
+			}
+		}
+#endif
+		#endregion
+
+		#endregion
 		#region Construction
 
-		// public Timer(float duration)
+		// public Timer(int loopCount, float duration)
 		// {
+		// 	this._isPlaying = false;
+		// 	this._whenStarted = 0f;
+
+		// 	this.loopCount = loopCount;
 		// 	this.duration = duration;
+		// 	this._Curves = new AnimationCurve[0];
+
+		// 	this._OnStart = new UnityEvent();
+		// 	this._OnCease = new UnityEvent();
+		// 	this._OnUpdate = new UnityEvent<float>();
+		// 	this._OnCycle = new UnityEvent<int>();
 		// }
 		// public Timer()
 		// {
@@ -47,13 +115,23 @@ namespace Mithril
 
 		#region Loops
 
+		// /// <summary>
+		// /// The number of loops to perform before stopping. Set to 0 for infinite loops.
+		// ///</summary>
+		// [Tooltip("The number of loops to perform before stopping. Set to 0 for infinite loops.")]
+		// [SerializeField]
+
+		// // public int loopCount;
+		// public int loopCount = 1;
+
 		/// <summary>
-		/// The number of loops to perform before stopping. Set to 0 for infinite loops.
+		/// Whether or not this timer should loop once started.
 		///</summary>
-		[Tooltip("The number of loops to perform before stopping. Set to 0 for infinite loops.")]
+
+		[Tooltip("Whether or not this timer should loop once started.")]
 		[SerializeField]
 
-		public int loopCount = 1;
+		public bool isLooping = false;
 
 		#endregion
 
@@ -65,7 +143,8 @@ namespace Mithril
 		[Tooltip("The overall duration of this Timer. OnCease is invoked once this amount of time has elapsed.")]
 		[SerializeField]
 
-		public float duration = 0f;
+		// public float duration;
+		public float duration = 1.0f;
 
 		#endregion
 
@@ -77,8 +156,8 @@ namespace Mithril
 		[Tooltip("The list of curves that this Timer oversees.")]
 		[SerializeField]
 
-		private AnimationCurve[] _Curves = new AnimationCurve[0];
-		public AnimationCurve[] curves => _Curves;
+		// public AnimationCurve curve;
+		public AnimationCurve curve = AnimationCurve.Linear(0f, 0f, 1f, 1f);
 
 		#endregion
 
@@ -90,6 +169,7 @@ namespace Mithril
 		[Tooltip("This event is called when this Timer is manually Started.")]
 		[SerializeField]
 
+		// private UnityEvent _OnStart;
 		private UnityEvent _OnStart = new UnityEvent();
 		public UnityEvent OnStart => _OnStart;
 
@@ -102,6 +182,7 @@ namespace Mithril
 		[Tooltip("This event is called after this Timer has started and has reached its full duration.")]
 		[SerializeField]
 
+		// private UnityEvent _OnCease;
 		private UnityEvent _OnCease = new UnityEvent();
 		public UnityEvent OnCease => _OnCease;
 
@@ -114,6 +195,7 @@ namespace Mithril
 		[Tooltip("This event is called each time Update is called and this Timer is playing.. The current time is passed as a parameter.")]
 		[SerializeField]
 
+		// private UnityEvent<float> _OnUpdate;
 		private UnityEvent<float> _OnUpdate = new UnityEvent<float>();
 		public UnityEvent<float> OnUpdate => _OnUpdate;
 
@@ -126,18 +208,19 @@ namespace Mithril
 		[Tooltip("This event is called each time this Timer has started and has reached its duration, if it is set to loop.")]
 		[SerializeField]
 
+		// private UnityEvent<int> _OnCycle;
 		private UnityEvent<int> _OnCycle = new UnityEvent<int>();
-		public UnityEvent<int> OnCycle = new UnityEvent<int>();
+		public UnityEvent<int> OnCycle => _OnCycle;
 
 		#endregion
 
 		#endregion
 		#region Members
 
-		private bool _isPlaying;
+		private bool _isPlaying = false;
 		public bool isPlaying => _isPlaying;
 
-		private float _whenStarted;
+		private float _whenStarted = 0f;
 
 		#endregion
 		#region Properties
@@ -226,15 +309,18 @@ namespace Mithril
 		#endregion
 		#region Evaluate
 
-		/// <summary>
-		/// This function returns the corresponding Y value at the given <see cref="currentTime"/> for the provided Curve index.
-		///</summary>
-		/// <param name="i">
-		/// The index of the Curve which to evaluate.
-		///</param>
+		public float curveValue =>
+			curve.Evaluate(currentTime);
 
-		public float Evaluate(int i = 0) =>
-			_Curves[i].Evaluate(currentTime);
+		// /// <summary>
+		// /// This function returns the corresponding Y value at the given <see cref="currentTime"/> for the provided Curve index.
+		// ///</summary>
+		// /// <param name="i">
+		// /// The index of the Curve which to evaluate.
+		// ///</param>
+
+		// public float Evaluate(int i = 0) =>
+		// 	_Curves[i].Evaluate(currentTime);
 
 		#endregion
 		#endregion
