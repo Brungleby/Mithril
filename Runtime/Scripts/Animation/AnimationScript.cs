fff06f7a9da5676d30f496ba52ead7ddaf3a0de4
William Wofford <27900656+Brungleby@users.noreply.github.com>
2023-11-24 10:14:09 -0600

    Added AnimationScript

---
+++ Runtime/Scripts/Animation/AnimationScript.cs
@@ -0,0 +1,31 @@
+
+/** AnimationScript.cs
+*
+*	Created by LIAM WOFFORD of CUBEROOT SOFTWARE, LLC.
+*
+*	Free to use or modify, with or without creditation,
+*	under the Creative Commons 0 License.
+*/
+
+#region Includes
+
+using UnityEngine;
+
+#endregion
+
+namespace Mithril.Animation
+{
+	#region AnimationScript
+
+	/// <summary>
+	/// Base class for a component used to control an animator.
+	///</summary>
+
+	public abstract class AnimationScript : MithrilComponent
+	{
+		[AutoAssign]
+		public Animator animator { get; private set; }
+	}
+
+	#endregion
+}
